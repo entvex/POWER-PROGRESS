ddfb16ccfa5230cb6a9ee8b29a836d23a546d217
HenrikBagger <black_volt@hotmail.com>
2016-10-03 14:04:35 +0200

    Added basic firebase upload, download not implemented yet.

--- PowerProgress/app/src/main/java/powerprogress/powerprogress/BasicDB.java
+++ PowerProgress/app/src/main/java/powerprogress/powerprogress/BasicDB.java
@@ -1,8 +1,36 @@
 package powerprogress.powerprogress;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.io.FileReader;
+
 /**
  * Created by bagge on 03-10-2016.
  */
 
 public class BasicDB {
+
+    private static final String FIREBASE_URL = "https://power-progress.firebaseio.com/";
+
+    private Firebase firebaseRef;
+
+    public BasicDB(Context context)
+    {
+        Firebase.setAndroidContext(context);
+        firebaseRef = new Firebase(FIREBASE_URL);
+    }
+
+    public void testInserter(String inputFolder,String inputText){
+        firebaseRef.child(inputFolder).setValue(inputText);
+    }
+
+    public void testReceiver(){
+        return;
+    }
 }
--- PowerProgress/app/src/main/java/powerprogress/powerprogress/MainMenuActivity.java
+++ PowerProgress/app/src/main/java/powerprogress/powerprogress/MainMenuActivity.java
@@ -2,12 +2,23 @@ package powerprogress.powerprogress;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainMenuActivity extends AppCompatActivity {
+TextView helloTextTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        helloTextTest = (TextView)findViewById(R.id.test);
+
+        BasicDB testSubject = new BasicDB(this);
+        testSubject.testInserter("folder","hey");
+        
+        testSubject.testReceiver();
     }
 }
