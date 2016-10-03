51787a8eccaaf6924cbb28b43f693149fa32eaff
HenrikBagger <black_volt@hotmail.com>
2016-10-03 13:11:29 +0200

    Project setup, ready to code

---
+++ PowerProgress/app/src/main/java/powerprogress/powerprogress/ProfileLoginDB.java
@@ -0,0 +1,8 @@
+package powerprogress.powerprogress;
+
+/**
+ * Created by bagge on 03-10-2016.
+ */
+
+public class ProfileLoginDB {
+}
---
+++ PowerProgress/app/src/main/java/powerprogress/powerprogress/UploadDataActivity.java
@@ -0,0 +1,13 @@
+package powerprogress.powerprogress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_data);
+    }
+}
